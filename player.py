1a23a8993e3bfd1c770d06b621f35fee8a9dab4d
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 10:37:22 -0500

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,12 @@
+class Player:
+    def __init__(self, name, position, age, rating, potential, value, wage):
+        self.name = name
+        self.position = position
+        self.age = age
+        self.rating = rating
+        self.potential = potential
+        self.value = value
+        self.wage = wage
 
+    def info(self):
+        return f"{self.name} | {self.position} | Rating: {self.rating}"
