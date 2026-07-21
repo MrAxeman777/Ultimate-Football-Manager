2f34486fcb15e44e7eb271178923a17e3277d3ea
MrAxeman777 <arsharma892@gmail.com>
2026-07-21 10:44:23 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,17 @@
+import json
 
+def load_players():
+    with open("database/players.json", "r") as file:
+        return json.load(file)
+
+
+def load_clubs():
+    with open("database/clubs.json", "r") as file:
+        return json.load(file)
+
+
+players = load_players()
+clubs = load_clubs()
+
+print(players)
+print(clubs)
